3196433412a03b4f0334a00459adad0648fd8729
AllanJetella <ajetella947@gmail.com>
2026-02-22 21:54:37 -0600

    clase docente

---
+++ Docente.cpp
@@ -0,0 +1,49 @@
+#include "Persona.cpp"
+#include <iostream>
+class Docente : public Persona {
+private:
+    string codigo;
+    string nit;
+    float salario;
+    string profesion;
+
+public:
+    // Constructor
+    Docente() {}
+
+    Docente(string cui, string nombres, string apellidos,
+            string direccion, int telefono,
+            string fecha_nacimiento, bool genero,
+            string codigo, string nit,
+            float salario, string profesion)
+        : Persona(cui, nombres, apellidos, direccion,
+                  telefono, fecha_nacimiento, genero) {
+
+        this->codigo = codigo;
+        this->nit = nit;
+        this->salario = salario;
+        this->profesion = profesion;
+    }
+
+    // Setters
+    void setCodigo(string codigo) { this->codigo = codigo; }
+    void setNit(string nit) { this->nit = nit; }
+    void setSalario(float salario) { this->salario = salario; }
+    void setProfesion(string profesion) { this->profesion = profesion; }
+
+    // Getters
+    string getCodigo() { return codigo; }
+    string getNit() { return nit; }
+    float getSalario() { return salario; }
+    string getProfesion() { return profesion; }
+
+    // Leer
+    void leer() {
+        Persona::leer();
+        cout << "\n--- Datos Docente ---" << endl;
+        cout << "Codigo: " << codigo << endl;
+        cout << "NIT: " << nit << endl;
+        cout << "Salario: " << salario << endl;
+        cout << "Profesion: " << profesion << endl;
+    }
+};
